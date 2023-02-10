96277135786d02b117aae4d0eb23614e57d610f1
MikeMaroni9 <k.vaitka@gmail.com>
2023-02-10 03:09:30 +0000

    game logic added

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,9 @@
 /**
  * Dom Caching and login form creation 
  */
+document.getElementById("ROCK").onclick=pickRock;
+document.getElementById("PAPER").onclick=pickPaper;
+document.getElementById("SCISSORS").onclick=pickScissors;
 document.getElementById('yourName');
 yourName.addEventListener('submit', getFormDetails);
 
@@ -15,3 +18,35 @@ function getFormDetails(event) {
     document.getElementById('name-result').textContent = name.value;
 }
 
+/**
+ * Random Number Generator
+ */
+
+function randomChoice(){
+	var randomNumber=Math.random();
+	var pcRandomChoice="ROCK";
+	if(randomNumber<0.33){
+		pcRandomChoice="SCISSORS";
+	}
+	else if(randomNumber<0.66){
+		pcRandomChoice="PAPER";
+	}
+	return pcRandomChoice;
+}
+
+/**
+ * Game Logic
+ */
+
+function pickRock(){
+	var pcRandomChoice=randomChoice();
+	checkWinner(pcRandomChoice,"ROCK");
+}
+function pickScissors(){
+	var pcRandomChoice=randomChoice();
+	checkWinner(pcRandomChoice,"SCISSORS");
+}
+function pickPaper(){
+	var pcRandomChoice=randomChoice();
+	checkWinner(pcRandomChoice,"PAPER");
+}
